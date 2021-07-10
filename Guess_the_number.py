d7b11bfed85346f31ab8113957a6971609f1f4f0
Daraujo-BR <daraujo.dac@gmail.com>
2021-07-10 16:13:22 -0300

    Update Guess_the_number.py

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -1,15 +1,29 @@
-#adivinhar o numero (ta perto? é multiplo, outras dicas)
 #Colocar as instruções
-#Colocar as outras dicas!
+
 import random
+#Creating the variable that saves the value of user!
 user = []
+#Computer choose the random number
 number = random.randint(1,100)
-print(number)
+#Loop while user doesn't got the right number
 while user!= number: 
+    #User try to discover the number by the input
     user = int(input('What number you think it is?'))
+    #Conditional if number is even
+    if number % 2 == 0:
+        print('The number is even!')
+    #Conditional if number is odd
+    else:
+        print('The number is odd!')
+    #Conditional to know if number that user tried is multiple of the number computer choose
+    if number % user == 0:
+        print(f'{user} is a multiple of the number')
+    #Conditional to know if number that user tried is lower than the number computer choose
     if user < number:
         print('The number is higher! Give it another try! ')
+    #Conditional to know if number that user tried is higher than the number computer choose
     else: 
         print('The number is smaller! Give it another try! ')
+#Conditional if user got the right number
 if user == number:
         print('Congratulations!')
